8ce4b990c4dbdfaffe18af6952169adbca8b91e3
Mario Rogic <hello@mario.net.au>
2017-09-23 19:19:31 +0700

    Autogenerate Migrations.hs

--- evergreen/Migrations.hs
+++ evergreen/Migrations.hs
@@ -1,14 +1,14 @@
 module Migrations where
-
 import qualified Hilt.Postgres
 import Data.Text (Text)
-
-import qualified Schema_20170823221628_c1474a1f412a2b4bb8f7c24a4ab5b54398825258
-
+import qualified
+       Schema_20170823221628_c1474a1f412a2b4bb8f7c24a4ab5b54398825258
+import qualified
+       Schema_20170908170731_44b2a37edc1ba22c0539e5a9ead21c4b2adf5898
 
 allMigrations :: [(Text, Hilt.Postgres.Handle -> IO ())]
-allMigrations =
-  [ ( "c1474a1f412a2b4bb8f7c24a4ab5b54398825258"
-    , Schema_20170823221628_c1474a1f412a2b4bb8f7c24a4ab5b54398825258.migration
-    )
-  ]
+allMigrations
+  = [("c1474a1f412a2b4bb8f7c24a4ab5b54398825258",
+      Schema_20170823221628_c1474a1f412a2b4bb8f7c24a4ab5b54398825258.migration),
+     ("44b2a37edc1ba22c0539e5a9ead21c4b2adf5898",
+      Schema_20170908170731_44b2a37edc1ba22c0539e5a9ead21c4b2adf5898.migration)]
---
+++ types/ShellHelpers.hs
@@ -0,0 +1,53 @@
+module ShellHelpers where
+
+import Language.Haskell.Exts.Simple
+import Turtle
+import Filesystem.Path.CurrentOS (fromText)
+
+import Data.Char (toLower)
+import qualified Data.Text as T
+import Data.Text.Encoding as E
+import Crypto.Hash
+
+
+(|>) = (&)
+
+
+loadFileAst :: Turtle.FilePath -> IO Module
+loadFileAst filepath = fromParseResult <$> parseFile (asString filepath)
+
+
+fileAstSha :: Turtle.FilePath -> IO Text
+fileAstSha file = do
+  ast <- loadFileAst file
+  pure $ astSha ast
+
+
+astSha :: Module -> Text
+astSha ast = tShow $ sha1 $ tShow ast
+
+
+sha1 :: Text -> Digest SHA1
+sha1 = hash . E.encodeUtf8
+
+
+asText :: Turtle.FilePath -> Text
+asText = format fp
+
+asString :: Turtle.FilePath -> String
+asString = T.unpack . asText
+
+tShow :: Show a => a -> Text
+tShow = T.pack . show
+
+
+doNothing :: IO ()
+doNothing = pure ()
+
+
+gs :: MonadIO io => io ()
+gs = stdout $ inshell "git status" empty
+
+
+lowercase :: String -> String
+lowercase = map toLower
